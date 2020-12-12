54ead1411597322aebe37d4ebd48ec10e9aef58b
Aleksandr Danilenko <rorik302@gmail.com>
2020-12-12 13:49:11 +0300

    __str__, status default

--- bonus_cards/models.py
+++ bonus_cards/models.py
@@ -6,11 +6,15 @@ class BonusCard(models.Model):
     number = models.CharField(max_length=50)
     created_at = models.DateField(auto_now_add=True)
     expired_at = models.DateField()
-    used_at = models.DateField()
+    used_at = models.DateField(null=True, blank=True)
     amount = models.DecimalField(decimal_places=2, max_digits=10)
     status = models.CharField(max_length=20,
                               choices=[
                                   ("not activated", "не активирована"),
                                   ("activated", "активирована"),
                                   ("expired", "просрочена")
-                              ])
+                              ],
+                              default="not activated")
+
+    def __str__(self):
+        return 'Bonus card {} № {}'.format(self.serial, self.number)
